246002b62c8dad125e39f7d54d752e81d9ae67df
Aakash Madhav Rao <mrao.aakash@gmail.com>
2023-11-25 14:26:15 +0530

    added preds

--- object-detection/benchmarking/detectron/detectron_train_v1.py
+++ object-detection/benchmarking/detectron/detectron_train_v1.py
@@ -157,7 +157,7 @@ cfg.SOLVER.MAX_ITER = max_iters
 cfg.SOLVER.STEPS = []        
 cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = 4 
-cfg.OUTPUT_DIR = os.path.join(data_path, f'output_{fold}_{name}_{version}')
+cfg.OUTPUT_DIR = os.path.join(data_path, f'fold_{fold}_{name}_{version}')
 
 
 os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
