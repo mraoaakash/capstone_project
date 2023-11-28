05b4b28e27c59100d8e118f8415020795c2700eb
Aakash Rao <mrao.aakash@gmail.com>
2023-11-28 16:10:10 +0530

    added

--- evaluations/overlay.py
+++ evaluations/overlay.py
@@ -4,9 +4,13 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-gt_txt_path = '/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_project/object-detection/benchmarking/datasets/NuCLS/folds/converted/ground_truth/annotations/JP.1_#_E_#_TCGA-A1-A0SP-01Z-00-DX1_id-5e83b16eddda5f83987d646e_left-8066_top-55982_bottom-56288_right-8365.txt'
-pred_txt_path = '/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_project/object-detection/benchmarking/datasets/NuCLS/folds/converted/faster_rcnn_R_50_C4_1x_fold_1/JP.1_#_E_#_TCGA-A1-A0SP-01Z-00-DX1_id-5e83b16eddda5f83987d646e_left-8066_top-55982_bottom-56288_right-8365.txt'
-img_path = '/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_project/object-detection/benchmarking/datasets/NuCLS/folds/converted/ground_truth/images/JP.1_#_E_#_TCGA-A1-A0SP-01Z-00-DX1_id-5e83b16eddda5f83987d646e_left-8066_top-55982_bottom-56288_right-8365.png'
+base_path = '/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_project/object-detection/benchmarking/datasets/NuCLS/folds/converted'
+# img = 'JP.1_#_E_#_TCGA-A1-A0SP-01Z-00-DX1_id-5e83b16eddda5f83987d646e_left-8066_top-55982_bottom-56288_right-8365'
+img = 'JP.1_#_E_#_TCGA-A1-A0SP-01Z-00-DX1_id-5e83b16eddda5f83987d646e_left-9067_top-55759_bottom-56058_right-9369'
+
+gt_txt_path = f'{base_path}/ground_truth/annotations/{img}.txt'
+pred_txt_path = f'{base_path}/faster_rcnn_R_50_C4_1x_fold_1/{img}.txt'
+img_path = f'{base_path}/ground_truth/images/{img}.png'
 
 # read txt files
 gt = pd.read_csv(gt_txt_path, sep=' ', header=None)
@@ -26,6 +30,11 @@ for i in range(len(gt)):
     y1 = gt.iloc[i]['y1']
     x2 = gt.iloc[i]['x2']
     y2 = gt.iloc[i]['y2']
+    # convert to int
+    x1 = int(x1)
+    y1 = int(y1)
+    x2 = int(x2)
+    y2 = int(y2)
     cv2.rectangle(im, (x1, y1), (x2, y2), (0, 255, 0), 2)
 # pred
 for i in range(len(pred)):
@@ -33,6 +42,11 @@ for i in range(len(pred)):
     y1 = pred.iloc[i]['y1']
     x2 = pred.iloc[i]['x2']
     y2 = pred.iloc[i]['y2']
+    # convert to int
+    x1 = int(x1)
+    y1 = int(y1)
+    x2 = int(x2)
+    y2 = int(y2)
     cv2.rectangle(im, (x1, y1), (x2, y2), (0, 0, 255), 2)
 plt.imshow(im)
 plt.show()
